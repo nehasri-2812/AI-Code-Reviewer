9e9ca01768e6c80fdcd74b94bd10d155e22aa6bb
nehasri-2812 <srineha651@gmail.com>
2026-06-14 14:56:27 +0530

    Configured backend scripts and dependencies

---
+++ backend/src/server.js
@@ -0,0 +1,37 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import connectDB from "./config/database.js";
+
+
+dotenv.config();
+
+connectDB();
+
+
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/",(req,res)=>{
+
+res.json({
+message:"AI Code Reviewer Running 🚀"
+});
+
+});
+
+
+app.listen(
+process.env.PORT,
+()=>{
+
+console.log(
+`Server running on ${process.env.PORT}`
+);
+
+});
